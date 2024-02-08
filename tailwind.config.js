1f4d92a50808f0e593fe50ae485964b6e6f1d3c4
wSeungMi <seungmi.dev@gmail.com>
2024-02-08 16:07:31 +0900

    Chore: tailwind.config theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#fff',
+      },
+      fontSize: {
+        base: '14px',
+      },
+      fontFamily: {
+        'LINESeedKR-Rg': ['LINESeedKR-Rg'],
+        'LINESeedKR-Bd': ['LINESeedKR-Bd'],
+      },
+      backgroundImage: {
+        'gradient-purple': 'linear-gradient(135deg,#9070c0 0%, #8fa5e5 100%)',
+        checkbox_off: "url('assets/checkbox_off.svg')",
+        checkbox_on: "url('assets/checkbox_on.svg')",
+      },
+      variants: {
+        fill: ['hover', 'focus'],
+      },
+    },
   },
   plugins: [],
 };
