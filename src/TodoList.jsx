964002bb2ad87f99ef95e3671ec4bd1b30a91870
wSeungMi <seungmi.dev@gmail.com>
2024-02-08 16:16:23 +0900

    Design: 기본 스타일 구현 - title, background, input, button - todo 기본 리스트, fontAwesome icon button

---
+++ src/TodoList.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+
+const TodoList = () => {
+  return (
+    <div className="w-full min-h-screen bg-gradient-purple flex flex-col justify-center items-center">
+      <section className="min-w-[600px] h-[540px] my-0 mx-auto py-7 px-8 bg-white border rounded-3xl">
+        <h1 className="mb-2.5 text-[24px] font-LINESeedKR-Bd">To Do List</h1>
+        <h2 className="ir">투두리스트 입력창</h2>
+        <div className="h-[50px]  mb-2.5 flex justify-between bg-[#eaeaea] rounded-3xl text-base">
+          <input
+            type="text"
+            placeholder="할일을 입력해주세요."
+            className="flex-1 p-5  font-LINESeedKR-Rg bg-transparent placeholder:text-gray-500"
+          />
+          <button
+            type="button"
+            className="w-[90px] text-white bg-[#9070c0] rounded-3xl hover:bg-[#584278]"
+          >
+            등록하기
+          </button>
+        </div>
+        <ul id="todo_list" className="pt-1 px-3.5 pb-1.5">
+          <li className="p-1.5 text-base cursor-pointer select-none flex justify-between items-center">
+            <div className="flex items-center">
+              <div className="w-5 h-5 bg-checkbox_off rounded-[50%] cursor-pointer bg-cover"></div>
+              {/* TODO: 할일 완료 기능 구현시 사용 예정 */}
+              {/* <div className="w-5 h-5 bg-checkbox_on rounded-[50%] cursor-pointer bg-cover relative"></div> */}
+              <span className="ml-3">자바스크립트 공부</span>
+            </div>
+
+            <div className="flex content-center items-center pl-4">
+              <button className="cursor-pointer pl-4">
+                <FontAwesomeIcon
+                  icon={faPenToSquare}
+                  className="hover:text-zinc-500"
+                />
+              </button>
+              <button className="cursor-pointer pl-3">
+                <FontAwesomeIcon
+                  icon={faXmark}
+                  size="xl"
+                  className="hover:text-zinc-500"
+                />
+              </button>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default TodoList;
