a75564b2cef8ee931c4d8362a39bbb3ab4efd9f2
wSeungMi <seungmi.dev@gmail.com>
2024-02-16 10:28:26 +0900

    Refactor: TodoList 컴포넌트 조회 구현 - 로컬 스토리지에 저장된 값 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoCreate from './components/TodoCreate';
 import TodoList from './components/TodoList';
 import './App.css';
 
+function initStoreTodos() {
+  const getTodos = JSON.parse(localStorage.getItem('my-todo'));
+  return getTodos ? getTodos : [];
+}
 function App() {
+  const [todo, setTodo] = useState(initStoreTodos);
+
   return (
     <>
       <div className="w-full min-h-screen bg-gradient-purple flex flex-col justify-center items-center">
         <TodoTemplate>
           <TodoCreate />
-          <TodoList />
+          <TodoList todoList={todo} />
         </TodoTemplate>
       </div>
     </>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,56 @@
-export default function TodoList() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faXmark,
+  faPenToSquare,
+  faCheck,
+  faTrash,
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+
+export default function TodoList({ todoList }) {
   return (
     <>
-      <p>TodoList</p>
+      <ul id="todo_list" className="pt-1 px-3.5 pb-1.5 h-[380px]">
+        {todoList.map((list) => (
+          <li
+            key={list.id}
+            className="px-1.5 py-2 text-base cursor-pointer select-none flex items-center "
+          >
+            <div className="flex justify-between w-full">
+              <>
+                <div className="flex flex-row">
+                  <div
+                    className={`w-5 h-5 rounded-[50%] cursor-pointer bg-cover ${list.done ? 'bg-checkbox_on' : 'bg-checkbox_off'}`}
+                  ></div>
+                  <span
+                    className={`ml-3 ${list.done ? 'line-through text-zinc-400' : ''}`}
+                  >
+                    {list.content}
+                  </span>
+                </div>
+
+                <div className="flex">
+                  <button className="cursor-pointer pl-4">
+                    <FontAwesomeIcon
+                      icon={faPenToSquare}
+                      size="lg"
+                      className="hover:text-zinc-500"
+                    />
+                  </button>
+                  <button className="cursor-pointer pl-3">
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      size="lg"
+                      className="hover:text-zinc-500"
+                    />
+                  </button>
+                </div>
+              </>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
