6507bfd9d02509163ee3fd35057c43b014de2e51
wSeungMi <seungmi.dev@gmail.com>
2024-02-16 13:47:31 +0900

    Refactor: TodoCreate 컴포넌트 할일 등록 구현 - enter 이벤트 등록, auto focusing 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import TodoTemplate from './components/TodoTemplate';
 import TodoCreate from './components/TodoCreate';
 import TodoList from './components/TodoList';
@@ -11,11 +12,23 @@ function initStoreTodos() {
 function App() {
   const [todo, setTodo] = useState(initStoreTodos);
 
+  const onCreate = (taskText) => {
+    setTodo([...todo, { id: uuid(), content: taskText, done: false }]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('my-todo', JSON.stringify(todo));
+
+    if (todo.length === 0) {
+      localStorage.removeItem('my-todo');
+    }
+  }, [todo]);
+
   return (
     <>
       <div className="w-full min-h-screen bg-gradient-purple flex flex-col justify-center items-center">
         <TodoTemplate>
-          <TodoCreate />
+          <TodoCreate onCreate={onCreate} />
           <TodoList todoList={todo} />
         </TodoTemplate>
       </div>
--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -1,7 +1,53 @@
-export default function TodoCreate() {
+import { useEffect, useRef, useState } from 'react';
+
+export default function TodoCreate({ onCreate }) {
+  const [taskText, setTaskText] = useState('');
+  const addInputRef = useRef(null);
+
+  const handleSubmit = () => {
+    if (taskText.length < 1) {
+      addInputRef.current.focus();
+      return;
+    }
+    onCreate(taskText);
+    setTaskText('');
+  };
+
+  const handleKeyDown = (e, action) => {
+    if (e.nativeEvent.isComposing) return;
+    if (e.key === 'Enter' && action === 'add') {
+      onCreate(taskText);
+      setTaskText('');
+    }
+  };
+
+  useEffect(() => {
+    if (addInputRef.current) {
+      addInputRef.current.focus();
+    }
+  }, [taskText]);
+
   return (
     <>
-      <p>TodoCreate</p>
+      <h2 className="ir">투두리스트 입력창</h2>
+      <div className="h-[50px]  mb-2.5 flex justify-between bg-[#eaeaea] rounded-3xl text-base">
+        <input
+          type="text"
+          placeholder="할일을 입력해주세요."
+          value={taskText}
+          ref={addInputRef}
+          onChange={(e) => setTaskText(e.target.value)}
+          onKeyDown={(e) => handleKeyDown(e, 'add', '')}
+          className="flex-1 p-5  font-LINESeedKR-Rg bg-transparent placeholder:text-gray-500"
+        />
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="w-[90px] text-white bg-[#9070c0] rounded-3xl hover:bg-[#584278]"
+        >
+          등록하기
+        </button>
+      </div>
     </>
   );
 }
