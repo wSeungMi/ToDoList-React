672c31f93c336784e793845cf90fd4751f36fc0a
wSeungMi <seungmi.dev@gmail.com>
2024-02-28 17:42:21 +0900

    Feat: 헤더에 현재 날짜 추가

--- src/components/TodoTemplate.jsx
+++ src/components/TodoTemplate.jsx
@@ -1,8 +1,17 @@
 export default function TodoWrapper({ children }) {
+  const today = new Date();
+  const dateString = today.toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+  });
   return (
     <>
-      <section className="min-w-[600px] h-[580px] my-0 mx-auto py-7 px-8 bg-white border rounded-3xl">
-        <h1 className="mb-2.5 text-[24px] font-LINESeedKR-Bd">To Do List</h1>
+      <section className="min-w-[600px] h-[620px] my-0 mx-auto py-7 px-8 bg-white border rounded-3xl">
+        <h1 className="mt-2 mb-6 text-[24px] font-LINESeedKR-Bd">
+          {dateString}
+        </h1>
         {children}
       </section>
     </>
